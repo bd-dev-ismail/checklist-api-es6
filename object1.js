9f9f6fef5265ffcc48595bcad1af244a05ac8e9e
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-01 14:35:36 +0600

    es6 object

---
+++ object1.js
@@ -0,0 +1,27 @@
+/**
+ * ১. একটা অবজেক্ট ডিক্লেয়ার করো। সেই অবজেক্ট এর মিনিমাম পাঁচটা প্রপার্টি থাকবে। তারপর একটা প্রপার্টি এর মান হবে অন্য আরেকটা অবজেক্ট , আরেকটা প্রপার্টি এর মান হবে একটা array । একটা প্রপার্টি এর মান হবে একটা ফাংশন(মেথড) । এই মেথড কে কল করলে সে মেথড এর ভিতর থেকে অবজেক্ট এর যেকোন একটা প্রপার্টি রিটার্ন করবে। আর বাকি দুইটা তোমার ইচ্ছা মতো। 
+ */
+const student = {
+    name: 'Ismail',
+    salary: 5000,
+    isHoenst: true,
+    address: [
+        {
+            village: 'Kazi para',
+            post: 'Kumira',
+            upzilla: 'Sitakunda',
+            zilla: 'Chattogram',
+        }
+    ],
+    homeVillage: function (){
+       return this.address[0].village + ` village is ${this.name} Home Town`;
+    },
+    forFamily: function(amuount, medicine){
+       return this.salary = this.salary - amuount - medicine;
+    }
+};
+// console.log(student);
+const result = student.homeVillage();
+console.log(result);
+const cost = student.forFamily(2000, 100);
+console.log(cost)
